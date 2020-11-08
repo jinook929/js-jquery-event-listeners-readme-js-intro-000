99208e27ed13ecfcc728dd61087720ef6597ea65
JINOOK JUNG <jinook929@gmail.com>
2020-11-08 02:27:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,10 @@ function getIt() {
   $('p').on('click', e => { alert('Hey!') });
 }
 
+function frameIt() {
+  $('.tasty').on('load', )
+}
+
 $(document).ready(function(){
 
 // call functions here
